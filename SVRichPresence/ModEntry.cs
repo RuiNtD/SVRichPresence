7e00f6431bccf70374bd80575f0e9c6801a39d9e
Fayne Aldan <FayneAldan@gmail.com>
2018-05-15 21:05:20 -0600

    Change RPC library

--- SVRichPresence/ModEntry.cs
+++ SVRichPresence/ModEntry.cs
@@ -1,69 +1,78 @@
 ﻿using System;
+using DiscordRPC;
+using DiscordRPC.Message;
 using Microsoft.Xna.Framework;
 using StardewModdingAPI;
 using StardewModdingAPI.Events;
 using StardewModdingAPI.Utilities;
 using StardewValley;
-using static DiscordRpc;
 
 namespace SVRichPresence {
 	public class ModEntry : Mod {
 		private const string clientId = "444517509148966923";
+		DiscordRpcClient client;
 		ModConfig config;
 
 		public override void Entry(IModHelper helper) {
+			client = new DiscordRpcClient(clientId, "413150", false);
+			client.Initialize();
 			config = Helper.ReadConfig<ModConfig>();
-			EventHandlers handlers = new EventHandlers();
-			DiscordRpc.Initialize(clientId, ref handlers, false, "413150");
 			GameEvents.UpdateTick += DoUpdate;
 			SaveEvents.AfterLoad += SetTimestamp;
 			SaveEvents.AfterReturnToTitle += ResetTimestamp;
 		}
 
-		private long timestamp = 0;
+		private DateTime? timestamp;
 
 		protected override void Dispose(bool disposing) {
-			DiscordRpc.Shutdown();
+			client.Dispose();
 		}
 
 		private void SetTimestamp(object sender, EventArgs e) {
-			timestamp = DateTime.UtcNow.Subtract(new DateTime(1970, 1, 1)).Ticks /
-				TimeSpan.TicksPerSecond;
+			timestamp = DateTime.UtcNow;
 		}
 
 		private void ResetTimestamp(object sender, EventArgs e) {
-			timestamp = -1;
+			timestamp = null;
 		}
 
 		private void DoUpdate(object sender, EventArgs e) {
-			RichPresence presence = new RichPresence();
-			presence.largeImageText = Helper.Reflection.GetField<string>
-				(typeof(Game1), "debugPresenceString").GetValue();
-			if (Context.IsWorldReady) {
-				if (!Context.IsMultiplayer)
-					presence.state = "Playing Solo";
-				else if (Context.IsMainPlayer)
-					presence.state = "Hosting Co-op";
-				else
-					presence.state = "Playing Co-op";
-				presence.details = $"{FarmName()} ({Game1.player.Money}g)";
-				if (timestamp >= 0)
-					presence.startTimestamp = timestamp;
-				if (Context.IsMultiplayer) {
-					presence.partySize = Game1.numberOfPlayers();
-					presence.partyMax = Game1.getFarm()
-						.getNumberBuildingsConstructed("Cabin") + 1;
-					presence.partyId = Constants.SaveFolderName;
-				}
-				presence.smallImageKey = "weather_" + WeatherKey();
-				presence.largeImageKey = $"{Game1.currentSeason}_{FarmTypeKey()}";
-				presence.smallImageText = Date();
-			} else {
-				presence.state = "In Menus";
-				presence.smallImageKey = "default_small";
-				presence.largeImageKey = "default_large";
-			}
-			DiscordRpc.UpdatePresence(presence);
+			RichPresence presence;
+			string gamePresence = Helper.Reflection.GetField<string>
+						(typeof(Game1), "debugPresenceString").GetValue();
+			if (Context.IsWorldReady)
+				presence = new RichPresence {
+					Details = $"{FarmName()} ({Game1.player.Money}g)",
+					State =
+						(!Context.IsMultiplayer) ? "Playing Solo" :
+						(!Context.IsMainPlayer) ? "Hosting Co-op" :
+						"Playing Co-op",
+					Assets = new Assets {
+						LargeImageKey = $"{Game1.currentSeason}_{FarmTypeKey()}",
+						SmallImageKey = "weather_" + WeatherKey(),
+						LargeImageText = gamePresence,
+						SmallImageText = Date()
+					},
+					Timestamps = new Timestamps {
+						Start = timestamp
+					},
+					Party = (!Context.IsMultiplayer) ? null : new Party {
+						ID = Constants.SaveFolderName,
+						Size = Game1.numberOfPlayers(),
+						Max = Game1.getFarm()
+							.getNumberBuildingsConstructed("Cabin") + 1
+					}
+				};
+			else
+				presence = new RichPresence {
+					State = "In Menus",
+					Assets = new Assets {
+						SmallImageKey = "default_small",
+						LargeImageKey = "default_large",
+						LargeImageText = gamePresence
+					}
+				};
+			client.SetPresence(presence);
 		}
 
 		private string Date() {
