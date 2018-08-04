026d5c2fc24cb0b4ba37e7a70e3d82828a4731ba
Fayne Aldan <faynealdan@gmail.com>
2018-08-04 14:06:25 -0600

    Update assembly. Hopefully fixes Mac and Linux support.

--- SVRichPresence/Properties/AssemblyInfo.cs
+++ SVRichPresence/Properties/AssemblyInfo.cs
@@ -1,10 +1,6 @@
 ﻿using System.Reflection;
-using System.Runtime.CompilerServices;
 using System.Runtime.InteropServices;
 
-// General Information about an assembly is controlled through the following
-// set of attributes. Change these attribute values to modify the information
-// associated with an assembly.
 [assembly: AssemblyTitle("Stardew Valley Discord Rich Presence")]
 [assembly: AssemblyDescription("Shows your in-game state on your Discord profile.")]
 [assembly: AssemblyConfiguration("")]
@@ -14,23 +10,11 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
-// Setting ComVisible to false makes the types in this assembly not visible
-// to COM components.  If you need to access a type in this assembly from
-// COM, set the ComVisible attribute to true on that type.
 [assembly: ComVisible(false)]
 
-// The following GUID is for the ID of the typelib if this project is exposed to COM
 [assembly: Guid("09756636-fa49-4706-8951-4aeb7ccca411")]
 
-// Version information for an assembly consists of the following four values:
-//
-//      Major Version
-//      Minor Version
-//      Build Number
-//      Revision
-//
-// You can specify all the values or you can default the Build and Revision Numbers
-// by using the '*' as shown below:
-// [assembly: AssemblyVersion("1.0.*")]
 [assembly: AssemblyVersion("1.2.1.0")]
 [assembly: AssemblyFileVersion("1.2.1.0")]
+
+[assembly: DefaultDllImportSearchPaths(DllImportSearchPath.UseDllDirectoryForDependencies)]
