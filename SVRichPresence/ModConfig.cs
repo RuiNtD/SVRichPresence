a89c06df2711d101b31129a39f6382f3b539009d
Fayne Aldan <faynealdan@gmail.com>
2018-08-24 01:54:37 -0600

    Add API

--- SVRichPresence/ModConfig.cs
+++ SVRichPresence/ModConfig.cs
@@ -1,26 +1,25 @@
 ﻿using StardewModdingAPI;
-using System;
 
 namespace SVRichPresence {
 	class ModConfig {
 		public SButton ReloadConfigButton = SButton.F5;
-		public Boolean ShowGlobalPlayTime = false;
+		public bool ShowGlobalPlayTime = false;
 		public MenuPresence MenuPresence = new MenuPresence();
 		public GamePresence GamePresence = new GamePresence();
 	}
 	class MenuPresence {
-		public Boolean ForceSmallImage = false;
+		public bool ForceSmallImage = false;
 		public string Details = "";
 		public string State = "In Menus";
 		public string LargeImageText = "{{ Activity }}";
 		public string SmallImageText = "";
 	}
 	class GamePresence : MenuPresence {
-		public Boolean ShowSeason = true;
-		public Boolean ShowFarmType = true;
-		public Boolean ShowWeather = true;
-		public Boolean ShowPlayerCount = true;
-		public Boolean ShowPlayTime = true;
+		public bool ShowSeason = true;
+		public bool ShowFarmType = true;
+		public bool ShowWeather = true;
+		public bool ShowPlayerCount = true;
+		public bool ShowPlayTime = true;
 
 		public GamePresence() {
 			Details = "{{ FarmName }} | {{ Money }}";
--- SVRichPresence/RichPresenceMod.cs
+++ SVRichPresence/RichPresenceMod.cs
@@ -5,12 +5,12 @@ using StardewValley;
 using System;
 using System.Collections.Generic;
 using System.Linq;
-using System.Text.RegularExpressions;
 
 namespace SVRichPresence {
 	public class RichPresenceMod : Mod {
 		private const string applicationId = "444517509148966923";
 		private ModConfig config = new ModConfig();
+		private IRichPresenceAPI api;
 
 		public override void Entry(IModHelper helper) {
 #if DEBUG
@@ -35,6 +35,7 @@ namespace SVRichPresence {
 				Monitor.Log("AAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAA", LogLevel.Alert);
 			}
 #endif
+			api = new RichPresenceAPI(this);
 			DiscordRpc.EventHandlers handlers = new DiscordRpc.EventHandlers();
 			DiscordRpc.Initialize(applicationId, ref handlers, false, "413150");
 			Helper.ConsoleCommands.Add("DiscordRP_Reload",
@@ -47,26 +48,54 @@ namespace SVRichPresence {
 			Helper.ConsoleCommands.Add("DiscordRP_Format",
 				"Formats and prints a provided configuration string.",
 				(string command, string[] args) => {
-					string text = FormatText(String.Join(" ", args));
+					string text = api.FormatText(string.Join(" ", args));
 					Monitor.Log("Result: " + text, LogLevel.Info);
 				}
 			);
 			Helper.ConsoleCommands.Add("DiscordRP_Tags",
 				"Lists tags usable for configuration strings.",
 				(string command, string[] args) => {
-					IDictionary<string, string> tags = GetTags();
-					int longest = 0;
-					foreach (string key in tags.Keys)
-						longest = Math.Max(longest, key.Length);
-					IList<string> output = new List<String>(tags.Count + 1);
-					output.Add("Available Tags:");
-					foreach (KeyValuePair<string, string> pair in tags) {
-						string key = "{{ " + pair.Key;
-						string value = pair.Value;
-						string keyPad = key.PadLeft(longest + 3);
-						output.Add(keyPad + " }}: " + value);
+					IDictionary<string, string> tags =
+						string.Join("", args).ToLower().StartsWith("all") ?
+						api.ListTags("[NULL]", "[ERROR]") : api.ListTags(removeNull: false);
+					IDictionary<string, IDictionary<string, string>> groups =
+						new Dictionary<string, IDictionary<string, string>>();
+					foreach (KeyValuePair<string, string> tag in tags) {
+						string owner = api.GetTagOwner(tag.Key) ?? "Unknown-Mod";
+						if (!groups.ContainsKey(owner))
+							groups[owner] = new Dictionary<string, string>();
+						groups[owner][tag.Key] = tag.Value;
 					}
-					Monitor.Log(String.Join(Environment.NewLine, output), LogLevel.Info);
+					IList<string> output = new List<string>(tags.Count + groups.Count) {
+						"Available Tags:"
+					};
+					int longest = 0;
+					foreach (KeyValuePair<string, string> tag in groups[ModManifest.UniqueID])
+						if (tag.Value != null)
+							longest = Math.Max(longest, tag.Key.Length);
+					int nulls = 0;
+					foreach (KeyValuePair<string, string> tag in groups[ModManifest.UniqueID])
+						if (tag.Value is null) nulls++;
+						else output.Add("  {{ " + tag.Key.PadLeft(longest) + " }}: " + tag.Value);
+					foreach (KeyValuePair<string, IDictionary<string, string>> group in groups) {
+						if (group.Key == this.ModManifest.UniqueID)
+							continue;
+						string head = group.Value.Count + " tag";
+						if (group.Value.Count != 1)
+							head += "s";
+						head += " from " + (Helper.ModRegistry.Get(group.Key)?.Name ?? "an unknown mod");
+						output.Add(head);
+						longest = 0;
+						foreach (KeyValuePair<string, string> tag in group.Value)
+							if (tag.Value != null)
+								longest = Math.Max(longest, tag.Key.Length);
+						foreach (KeyValuePair<string, string> tag in group.Value)
+							if (tag.Value == null) nulls++;
+							else output.Add("  {{ " + tag.Key.PadLeft(longest) + " }}: " + tag.Value);
+					}
+					if (nulls > 0)
+						output.Add(nulls + " tag" + (nulls != 1 ? "s" : "") + " unavailable; type `DiscordRP_Tags all` to show all");
+					Monitor.Log(string.Join(Environment.NewLine, output), LogLevel.Info);
 				}
 			);
 			LoadConfig();
@@ -75,47 +104,91 @@ namespace SVRichPresence {
 			SaveEvents.AfterLoad += SetTimestamp;
 			SaveEvents.AfterReturnToTitle += SetTimestamp;
 			SaveEvents.AfterLoad += (object sender, EventArgs e) =>
-				GamePresence = "Getting Started";
+				api.GamePresence = "Getting Started";
 			SaveEvents.AfterCreate += (object sender, EventArgs e) =>
-				GamePresence = "Starting a New Game";
+				api.GamePresence = "Starting a New Game";
 			GameEvents.FirstUpdateTick += (object sender, EventArgs e) => {
 				SetTimestamp();
 				timestampSession = GetTimestamp();
 			};
+
+			ITagRegister tagReg = api.GetTagRegister(this);
+			
+			tagReg.SetTag("Activity", () => api.GamePresence);
+			tagReg.SetTag("ModCount", () => Helper.ModRegistry.GetAll().Count());
+			tagReg.SetTag("SMAPIVersion", () => Constants.ApiVersion.ToString());
+			tagReg.SetTag("StardewVersion", () => Game1.version);
+			tagReg.SetTag("Song", () => Utility.getSongTitleFromCueName(Game1.currentSong?.Name ?? api.None));
+
+			// All the tags below are only available while in-game.
+
+			tagReg.SetTag("Name", () => Game1.player.Name, true);
+			tagReg.SetTag("Farm", () => Game1.content.LoadString("Strings\\UI:Inventory_FarmName", api.GetTag("FarmName")), true);
+			tagReg.SetTag("FarmName", () => Game1.player.farmName, true);
+			tagReg.SetTag("PetName", () => Game1.player.hasPet() ? Game1.player.getPetDisplayName() : api.None, true);
+			tagReg.SetTag("Location", () => Game1.currentLocation.Name, true);
+			tagReg.SetTag("RomanticInterest", () => Utility.getTopRomanticInterest(Game1.player)?.getName() ?? api.None, true);
+			tagReg.SetTag("PercentComplete", () => Utility.percentGameComplete(), true);
+
+			tagReg.SetTag("Money", () => {
+				// Copied from LoadGameMenu
+				string text = Game1.content.LoadString("Strings\\StringsFromCSFiles:LoadGameMenu.cs.11020", Utility.getNumberWithCommas(Game1.player.Money));
+				if (Game1.player.Money == 1 && LocalizedContentManager.CurrentLanguageCode == LocalizedContentManager.LanguageCode.pt)
+					text = text.Substring(0, text.Length - 1);
+				return text;
+			}, true);
+			tagReg.SetTag("MoneyNumber", () => Game1.player.Money, true);
+			tagReg.SetTag("MoneyCommas", () => Utility.getNumberWithCommas(Game1.player.Money), true);
+			tagReg.SetTag("Level", () => Game1.content.LoadString("Strings\\UI:Inventory_PortraitHover_Level", Game1.player.Level.ToString()), true);
+			tagReg.SetTag("LevelNumber", () => Game1.player.Level, true);
+			tagReg.SetTag("Title", () => Game1.player.getTitle(), true);
+			tagReg.SetTag("TotalTime", () => Utility.getHoursMinutesStringFromMilliseconds(Game1.player.millisecondsPlayed), true);
+
+			tagReg.SetTag("Health", () => Game1.player.health, true);
+			tagReg.SetTag("HealthMax", () => Game1.player.maxHealth, true);
+			tagReg.SetTag("HealthPercent", () => (double) Game1.player.health / Game1.player.maxHealth * 100, 2, true);
+			tagReg.SetTag("Energy", () => Game1.player.Stamina.ToString(), true);
+			tagReg.SetTag("EnergyMax", () => Game1.player.MaxStamina, true);
+			tagReg.SetTag("EnergyPercent", () => (double) Game1.player.Stamina / Game1.player.MaxStamina * 100, 2, true);
+
+			tagReg.SetTag("Time", () => Game1.getTimeOfDayString(Game1.timeOfDay), true);
+			tagReg.SetTag("Date", () => Utility.getDateString(), true);
+			tagReg.SetTag("Season", () => Utility.getSeasonNameFromNumber(Utility.getSeasonNumber(SDate.Now().Season)), true);
+			tagReg.SetTag("DayOfWeek", () => Game1.shortDayDisplayNameFromDayOfSeason(SDate.Now().Day), true);
+
+			tagReg.SetTag("Day", () => SDate.Now().Day, true);
+			tagReg.SetTag("DayPad", () => $"{SDate.Now().Day:00}", true);
+			tagReg.SetTag("DaySuffix", () => Utility.getNumberEnding(SDate.Now().Day), true);
+			tagReg.SetTag("Year", () => SDate.Now().Year, true);
+			tagReg.SetTag("YearSuffix", () => Utility.getNumberEnding(SDate.Now().Year), true);
+
+			tagReg.SetTag("GameVerb", () =>
+				Context.IsMultiplayer && Context.IsMainPlayer ? "Hosting" : "Playing", true);
+			tagReg.SetTag("GameNoun", () => Context.IsMultiplayer ? "Co-op" : "Solo", true);
+			tagReg.SetTag("GameInfo", () => api.GetTag("GameVerb") + " " + api.GetTag("GameNoun"), true);
 		}
+
+		public override object GetApi() => api;
 
 		private void HandleButton(object sender, EventArgsInput e) {
 			if (e.Button != config.ReloadConfigButton)
 				return;
 			try {
 				LoadConfig();
-				Game1.addHUDMessage(new HUDMessage("DiscordRP config reloaded."));
+				Game1.addHUDMessage(new HUDMessage("DiscordRP config reloaded.", HUDMessage.newQuest_type));
 			} catch (Exception err) {
-				Game1.addHUDMessage(new HUDMessage("Failed to reload DiscordRP config. Check console.", 3));
+				Game1.addHUDMessage(new HUDMessage("Failed to reload DiscordRP config. Check console.", HUDMessage.error_type));
 				Monitor.Log(err.ToString(), LogLevel.Error);
 			}
 		}
 
-		private void LoadConfig() =>
-			config = Helper.ReadConfig<ModConfig>();
-
-		private void SaveConfig() =>
-			Helper.WriteConfig<ModConfig>(config);
-
-		private string GamePresence {
-			get => Helper.Reflection.GetField<string>
-				(typeof(Game1), "debugPresenceString").GetValue();
-			set => Helper.Reflection.GetField<string>
-				(typeof(Game1), "debugPresenceString").SetValue(value);
-		}
+		private void LoadConfig() => config = Helper.ReadConfig<ModConfig>();
+		private void SaveConfig() => Helper.WriteConfig<ModConfig>(config);
 
 		private long timestampSession = 0;
 		private long timestampFarm = 0;
-
-		private void SetTimestamp(object sender, EventArgs e) =>
-				SetTimestamp();
-		private void SetTimestamp() =>
-			timestampFarm = GetTimestamp();
+		private void SetTimestamp(object sender, EventArgs e) => SetTimestamp();
+		private void SetTimestamp() => timestampFarm = GetTimestamp();
 
 		private long GetTimestamp() {
 			DateTime epoch = new DateTime(1970, 1, 1, 0, 0, 0, DateTimeKind.Utc);
@@ -124,19 +197,17 @@ namespace SVRichPresence {
 
 		private void DoUpdate(object sender, EventArgs e) =>
 			DiscordRpc.UpdatePresence(GetPresence());
-		
-		private MenuPresence Conf {
-			get => !Context.IsWorldReady ? config.MenuPresence :
-				config.GamePresence;
-		}
+
+		private MenuPresence Conf => !Context.IsWorldReady ?
+			config.MenuPresence : config.GamePresence;
 
 		private DiscordRpc.RichPresence GetPresence() {
 			DiscordRpc.RichPresence presence = new DiscordRpc.RichPresence {
-				details = FormatText(Conf.Details),
-				state = FormatText(Conf.State),
+				details = api.FormatText(Conf.Details),
+				state = api.FormatText(Conf.State),
 				largeImageKey = "default_large",
-				largeImageText = FormatText(Conf.LargeImageText),
-				smallImageText = FormatText(Conf.SmallImageText)
+				largeImageText = api.FormatText(Conf.LargeImageText),
+				smallImageText = api.FormatText(Conf.SmallImageText)
 			};
 			if (Conf.ForceSmallImage)
 				presence.smallImageKey = "default_small";
@@ -158,96 +229,11 @@ namespace SVRichPresence {
 					presence.joinSecret = Game1.server.getInviteCode();
 				}
 			}
-			
+
 			if (config.ShowGlobalPlayTime)
 				presence.startTimestamp = timestampSession;
 
 			return presence;
-		}
-
-		private IDictionary<string, string> GetTags() {
-			int modCount = Helper.ModRegistry.GetAll().Count();
-
-			IEqualityComparer<string> comp = StringComparer.InvariantCultureIgnoreCase;
-			string none = Game1.content.LoadString("Strings\\UI:Character_none");
-			IDictionary<string, string> tags = new Dictionary<string, string>(comp) {
-				["Activity"] = GamePresence,
-				["ModCount"] = modCount.ToString(),
-				["SMAPIVersion"] = Constants.ApiVersion.ToString(),
-				["StardewVersion"] = Game1.version,
-				["Song"] = Utility.getSongTitleFromCueName(Game1.currentSong?.Name ?? none)
-			};
-
-			// All the tags below are only available while in a farm.
-			if (Context.IsWorldReady) {
-				SDate now = SDate.Now();
-				tags["Name"] = Game1.player.Name;
-				tags["Farm"] = Game1.content.LoadString("Strings\\UI:Inventory_FarmName", Game1.player.farmName.ToString());
-				tags["FarmName"] = Game1.player.farmName.ToString();
-				tags["PetName"] = Game1.player.hasPet() ? Game1.player.getPetDisplayName() : none;
-				tags["Location"] = Game1.currentLocation.Name;
-				tags["RomanticInterest"] = Utility.getTopRomanticInterest(Game1.player)?.getName() ?? none;
-				tags["PercentComplete"] = Utility.percentGameComplete().ToString();
-
-				{
-					// Copied from LoadGameMenu
-					string text = Game1.content.LoadString("Strings\\StringsFromCSFiles:LoadGameMenu.cs.11020", Utility.getNumberWithCommas(Game1.player.Money));
-					if (Game1.player.Money == 1 && LocalizedContentManager.CurrentLanguageCode == LocalizedContentManager.LanguageCode.pt)
-						text = text.Substring(0, text.Length - 1);
-					tags["Money"] = text;
-				}
-				tags["MoneyNumber"] = Game1.player.Money.ToString();
-				tags["MoneyCommas"] = Utility.getNumberWithCommas(Game1.player.Money);
-				tags["Level"] = Game1.content.LoadString("Strings\\UI:Inventory_PortraitHover_Level", Game1.player.Level.ToString());
-				tags["LevelNumber"] = Game1.player.Level.ToString();
-				tags["Title"] = Game1.player.getTitle();
-				tags["TotalTime"] = Utility.getHoursMinutesStringFromMilliseconds(Game1.player.millisecondsPlayed);
-
-				tags["Health"] = Game1.player.health.ToString();
-				tags["HealthMax"] = Game1.player.maxHealth.ToString();
-				tags["HealthPercent"] = Percent(Game1.player.health, Game1.player.maxHealth).ToString();
-				tags["Energy"] = Game1.player.Stamina.ToString();
-				tags["EnergyMax"] = Game1.player.MaxStamina.ToString();
-				tags["EnergyPercent"] = Percent(Game1.player.Stamina, Game1.player.MaxStamina).ToString();
-
-				tags["Time"] = Game1.getTimeOfDayString(Game1.timeOfDay);
-				tags["Date"] = Utility.getDateString();
-				tags["Season"] = Utility.getSeasonNameFromNumber(Utility.getSeasonNumber(now.Season));
-				tags["DayOfWeek"] = Game1.shortDayDisplayNameFromDayOfSeason(now.Day);
-
-				tags["Day"] = now.Day.ToString();
-				tags["DayPad"] = $"{now.Day:00}";
-				tags["DaySuffix"] = Utility.getNumberEnding(now.Day);
-				tags["Year"] = now.Year.ToString();
-				tags["YearSuffix"] = Utility.getNumberEnding(now.Year);
-				
-				tags["GameVerb"] = "Playing";
-				tags["GameNoun"] = "Co-op";
-				if (!Context.IsMultiplayer)
-					tags["GameNoun"] = "Solo";
-				else if (Context.IsMainPlayer)
-					tags["GameVerb"] = "Hosting";
-				tags["GameInfo"] = tags["GameVerb"] + " " + tags["GameNoun"];
-			}
-
-			return tags;
-		}
-
-		private string FormatText(string text) {
-			if (text.Length == 0)
-				return null;
-
-			// Code is copied and modified from SMAPI.
-			IDictionary<string, string> tags = GetTags();
-			return Regex.Replace(text, @"{{([ \w\.\-]+)}}", match => {
-				string key = match.Groups[1].Value.Trim();
-				return tags.TryGetValue(key, out string value)
-					? value : match.Value;
-			});
-		}
-
-		private double Percent(double current, double max) {
-			return Math.Round(current / max * 100, 2);
 		}
 
 		private string FarmTypeKey() {
