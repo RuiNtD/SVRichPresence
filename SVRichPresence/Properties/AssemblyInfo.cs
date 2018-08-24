274f4539695f2bf985fcadcdc087b451b3362c2b
Fayne Aldan <faynealdan@gmail.com>
2018-08-24 02:09:54 -0600

    Update name and version

--- SVRichPresence/Properties/AssemblyInfo.cs
+++ SVRichPresence/Properties/AssemblyInfo.cs
@@ -1,11 +1,12 @@
-﻿using System.Reflection;
+﻿using System.Resources;
+using System.Reflection;
 using System.Runtime.InteropServices;
 
-[assembly: AssemblyTitle("Stardew Valley Discord Rich Presence")]
+[assembly: AssemblyTitle("Stardew Valley Rich Presence")]
 [assembly: AssemblyDescription("Shows your in-game state on your Discord profile.")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("Stardew Valley Discord Rich Presence")]
+[assembly: AssemblyProduct("Stardew Valley Rich Presence")]
 [assembly: AssemblyCopyright("Copyright © Fayne Aldan 2018")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
@@ -14,5 +15,7 @@ using System.Runtime.InteropServices;
 
 [assembly: Guid("09756636-fa49-4706-8951-4aeb7ccca411")]
 
-[assembly: AssemblyVersion("1.2.1.0")]
-[assembly: AssemblyFileVersion("1.2.1.0")]
+[assembly: AssemblyVersion("2.1.0.0")]
+[assembly: AssemblyFileVersion("2.1.0.0")]
+[assembly: NeutralResourcesLanguage("en-US")]
+
