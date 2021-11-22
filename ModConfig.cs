196959deee0dfb268dedd6da9e4d43de4b36120c
Fayne Aldan <FayneAldan@gmail.com>
2021-11-21 23:53:04 -0700

    Format code

--- ModConfig.cs
+++ ModConfig.cs
@@ -1,31 +1,36 @@
 ﻿using StardewModdingAPI;
 
-namespace SVRichPresence {
-	internal class ModConfig {
-		public SButton ReloadConfigButton = SButton.F5;
-		public bool ShowGlobalPlayTime = false;
-		public MenuPresence MenuPresence = new MenuPresence();
-		public GamePresence GamePresence = new GamePresence();
-	}
+namespace SVRichPresence
+{
+    internal class ModConfig
+    {
+        public SButton ReloadConfigButton = SButton.F5;
+        public bool ShowGlobalPlayTime = false;
+        public MenuPresence MenuPresence = new MenuPresence();
+        public GamePresence GamePresence = new GamePresence();
+    }
 
-	internal class MenuPresence {
-		public bool ForceSmallImage = false;
-		public string Details = "";
-		public string State = "In Menus";
-		public string LargeImageText = "{{ Activity }}";
-		public string SmallImageText = "";
-	}
+    internal class MenuPresence
+    {
+        public bool ForceSmallImage = false;
+        public string Details = "";
+        public string State = "In Menus";
+        public string LargeImageText = "{{ Activity }}";
+        public string SmallImageText = "";
+    }
 
-	internal class GamePresence : MenuPresence {
-		public bool ShowSeason = true;
-		public bool ShowFarmType = true;
-		public bool ShowWeather = true;
-		public bool ShowPlayTime = true;
-		
-		public GamePresence() {
-			Details = "{{ FarmName }} | {{ Money }}";
-			State = "{{ GameInfo }}";
-			SmallImageText = "{{ Date }}";
-		}
-	}
+    internal class GamePresence : MenuPresence
+    {
+        public bool ShowSeason = true;
+        public bool ShowFarmType = true;
+        public bool ShowWeather = true;
+        public bool ShowPlayTime = true;
+
+        public GamePresence()
+        {
+            Details = "{{ FarmName }} | {{ Money }}";
+            State = "{{ GameInfo }}";
+            SmallImageText = "{{ Date }}";
+        }
+    }
 }
--- MonitorLogger.cs
+++ MonitorLogger.cs
@@ -4,34 +4,42 @@ using System;
 using RPCLogLevel = DiscordRPC.Logging.LogLevel;
 using SDVLogLevel = StardewModdingAPI.LogLevel;
 
-namespace SVRichPresence {
-	public class MonitorLogger : ILogger {
-		public RPCLogLevel Level { get; set; }
-		private readonly IMonitor Monitor;
+namespace SVRichPresence
+{
+    public class MonitorLogger : ILogger
+    {
+        public RPCLogLevel Level { get; set; }
+        private readonly IMonitor Monitor;
 
-		public MonitorLogger(IMonitor monitor) {
-			Monitor = monitor;
-		}
+        public MonitorLogger(IMonitor monitor)
+        {
+            Monitor = monitor;
+        }
 
-		public void Trace(string message, params object[] args) {
-			Log(message, args, SDVLogLevel.Trace);
-		}
+        public void Trace(string message, params object[] args)
+        {
+            Log(message, args, SDVLogLevel.Trace);
+        }
 
-		public void Info(string message, params object[] args) {
-			Log(message, args, SDVLogLevel.Info);
-		}
+        public void Info(string message, params object[] args)
+        {
+            Log(message, args, SDVLogLevel.Info);
+        }
 
-		public void Warning(string message, params object[] args) {
-			Log(message, args, SDVLogLevel.Warn);
-		}
+        public void Warning(string message, params object[] args)
+        {
+            Log(message, args, SDVLogLevel.Warn);
+        }
 
-		public void Error(string message, params object[] args) {
-			Log(message, args, SDVLogLevel.Error);
-		}
+        public void Error(string message, params object[] args)
+        {
+            Log(message, args, SDVLogLevel.Error);
+        }
 
-		private void Log(string message, object[] args, SDVLogLevel level) {
-			if (Monitor.IsVerbose)
-				Monitor.Log("DISCORD: " + String.Format(message, args), level);
-		}
-	}
+        private void Log(string message, object[] args, SDVLogLevel level)
+        {
+            if (Monitor.IsVerbose)
+                Monitor.Log("DISCORD: " + String.Format(message, args), level);
+        }
+    }
 }
