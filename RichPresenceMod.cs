dff3fc17569a1eaac7865ec9684c2884e2e133b4
RuiNtD <RuiNtD@outlook.com>
2024-03-23 15:49:24 -0600

    Use FormatTag

--- RichPresenceMod.cs
+++ RichPresenceMod.cs
@@ -140,7 +140,7 @@ namespace SVRichPresence {
 
       SetTag(mod, "GameVerb", ReqWorld(() => Context.IsMultiplayer && Context.IsMainPlayer ? "Hosting" : "Playing"));
       SetTag(mod, "GameNoun", ReqWorld(() => Context.IsMultiplayer ? "Co-op" : "Solo"));
-      SetTag(mod, "GameInfo", ReqWorld(() => api.ResolveTag("GameVerb").Value + " " + api.ResolveTag("GameNoun").Value));
+      SetTag(mod, "GameInfo", ReqWorld(() => api.FormatTag("GameVerb") + " " + api.FormatTag("GameNoun")));
       #endregion
     }
 
